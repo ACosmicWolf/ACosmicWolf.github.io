65dd8f3d11aced9e1731d0c9a86cae0878ea383d
ACosmicWolf <cosmicwolf009@gmail.com>
2021-12-24 22:54:40 +0530

    Added tooltips

--- main.js
+++ main.js
@@ -8,3 +8,17 @@ tippy('#projects', {
         content: 'Projects',
 	animation: 'fade',
 });
+
+
+tippy('#discord', {
+        content: 'Contact Me on Discord',
+	animation: 'fade',
+});
+tippy('#github', {
+        content: 'Github',
+	animation: 'fade',
+});
+tippy('#mail', {
+        content: 'Email',
+	animation: 'fade',
+});
